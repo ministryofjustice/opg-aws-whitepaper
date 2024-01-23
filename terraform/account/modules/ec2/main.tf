83f88b19359ca32b97d20497ba54f0ded7f2495d
Jay Whitwell <jay.whitwell@digital.justice.gov.uk>
2024-01-23 13:12:13 +0000

    assign sg to ec2

--- terraform/account/modules/ec2/main.tf
+++ terraform/account/modules/ec2/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "example" {
   ami           = "ami-0c45689cf7ad8a412"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.example_sg.id]
 
   root_block_device {
     encrypted = true
@@ -14,7 +15,7 @@ resource "aws_instance" "example" {
     echo "Hello, World!" > index.html
     nohup busybox httpd -f -p 8080 &
     EOF
-  
+
   user_data_replace_on_change = true
 
 
@@ -24,4 +25,15 @@ resource "aws_instance" "example" {
 
 
   provider = aws
+}
+
+resource "aws_security_group" "example_sg" {
+  name = "example-sg"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
