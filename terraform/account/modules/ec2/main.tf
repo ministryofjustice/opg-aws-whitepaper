86c3389b283af71969d5dbf234087fe9d21bf75e
Jay Whitwell <jay.whitwell@digital.justice.gov.uk>
2024-01-23 13:12:44 +0000

    fix format

--- terraform/account/modules/ec2/main.tf
+++ terraform/account/modules/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example" {
-  ami           = "ami-0c45689cf7ad8a412"
-  instance_type = "t2.micro"
+  ami                    = "ami-0c45689cf7ad8a412"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.example_sg.id]
 
   root_block_device {
