0365869bf6bce32f9f15819d041a0a2dad0b14de
Jay Whitwell <jay.whitwell@digital.justice.gov.uk>
2024-01-25 12:57:20 +0000

    add target group

--- terraform/account/modules/ec2/main.tf
+++ terraform/account/modules/ec2/main.tf
@@ -43,37 +43,6 @@ resource "aws_lb" "sandbox" {
   security_groups    = [aws_security_group.sandbox_alb.id]
 }
 
-
-resource "aws_security_group" "sandbox_sg" {
-  name = "sandbox-sg"
-
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-}
-
-resource "aws_security_group" "sandbox_alb" {
-  name = "sandbox_alb"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.sandbox.arn
   port              = 80
@@ -88,6 +57,53 @@ resource "aws_lb_listener" "http" {
       message_body = "404: page not found"
       status_code  = 404
     }
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "sandbox-asg"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_security_group" "sandbox_sg" {
+  name = "sandbox-sg"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_security_group" "sandbox_alb" {
+  name = "sandbox-alb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
