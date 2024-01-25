1c502ce28fd307869e2678556398a1825cdbd7cd
Jay Whitwell <jay.whitwell@digital.justice.gov.uk>
2024-01-25 10:04:01 +0000

    remove egress

--- terraform/account/modules/ec2/main.tf
+++ terraform/account/modules/ec2/main.tf
@@ -35,10 +35,4 @@ resource "aws_security_group" "example_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/1"]
-  }
 }
