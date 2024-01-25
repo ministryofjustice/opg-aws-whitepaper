72ccd43584e96860d78e98fe6637b3caf4c7fdc1
Jay Whitwell <jay.whitwell@digital.justice.gov.uk>
2024-01-25 11:40:46 +0000

    create launch config

--- terraform/account/modules/ec2/main.tf
+++ terraform/account/modules/ec2/main.tf
@@ -1,7 +1,7 @@
-resource "aws_instance" "example" {
-  ami                    = "ami-0905a3c97561e0b69"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.example_sg.id]
+resource "aws_launch_configuration" "sandbox" {
+  image_id        = "ami-0905a3c97561e0b69"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.example_sg.id]
 
   root_block_device {
     encrypted = true
@@ -15,12 +15,6 @@ resource "aws_instance" "example" {
     echo "Hello, World!" > index.html
     nohup busybox httpd -f -p ${var.server_port} &
     EOF
-
-  user_data_replace_on_change = true
-
-  tags = {
-    Name = "temp-webserver"
-  }
 
   provider = aws
 }
