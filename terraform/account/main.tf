7bd002126f7fa249f69fb52ce05079f745449c0e
Jay Whitwell <jay.whitwell@digital.justice.gov.uk>
2024-01-30 14:46:45 +0000

    testing module

--- terraform/account/main.tf
+++ terraform/account/main.tf
@@ -1,5 +1,14 @@
 module "ec2" {
-  source = "./modules/ec2"
+  source       = "./modules/ec2"
+  cluster_name = "sandbox-staging"
+  providers = {
+    aws = aws.sandbox
+  }
+}
+
+module "ec2" {
+  source       = "./modules/ec2"
+  cluster_name = "sandbox-prp"
   providers = {
     aws = aws.sandbox
   }
