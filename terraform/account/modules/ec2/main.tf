95f9376a1d56a55f24823cbc35766d888c711ed3
Jay Whitwell <jay.whitwell@digital.justice.gov.uk>
2024-01-30 15:25:48 +0000

    use recommended sg rules

--- terraform/account/modules/ec2/main.tf
+++ terraform/account/modules/ec2/main.tf
@@ -121,10 +121,10 @@ resource "aws_security_group" "sandbox_alb" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [data.aws_vpc.main.cidr_block]
   }
 }
 
