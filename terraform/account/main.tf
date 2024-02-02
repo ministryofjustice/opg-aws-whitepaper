800df67c09f2540d2387dfae76a95e5c2a509919
Jay Whitwell <jay.whitwell@digital.justice.gov.uk>
2024-02-02 12:00:25 +0000

    fix again

--- terraform/account/main.tf
+++ terraform/account/main.tf
@@ -1,7 +1,7 @@
 module "loadbalancer" {
   source             = "./modules/alb"
   vpc_id             = data.aws_vpc.default.id
-  subnet_ids         = data.aws_subnets.ids
+  subnet_ids         = data.aws_subnets.default.ids
   availability_zones = data.aws_availability_zones.default.names
   providers = {
     aws = aws.sandbox
