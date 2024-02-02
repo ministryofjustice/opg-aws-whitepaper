b3e5c42c7a70be6df015682058f05342f48a2702
Jay Whitwell <jay.whitwell@digital.justice.gov.uk>
2024-02-02 12:08:24 +0000

    fix issues

--- terraform/account/modules/alb/locals.tf
+++ terraform/account/modules/alb/locals.tf
@@ -2,6 +2,4 @@ locals {
   http  = 80
   https = 443
   ssh   = 22
-
-  vpc_id = var.vpc_id != "" ? var.vpc_id : data.aws_vpc.default.id
 }
--- terraform/account/modules/alb/main.tf
+++ terraform/account/modules/alb/main.tf
@@ -43,7 +43,7 @@ resource "aws_lb_target_group" "sandbox_asg" {
   name     = "${var.web_cluster_name}-asg"
   port     = var.server_port
   protocol = "HTTP"
-  vpc_id   = local.vpc_id
+  vpc_id   = var.vpc_id
   health_check {
     path                = "/"
     protocol            = "HTTP"
