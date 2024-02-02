d9b970041824be9f9685de5f961d08daa5515dbb
Jay Whitwell <jay.whitwell@digital.justice.gov.uk>
2024-02-02 16:45:06 +0000

    create usable vars

--- terraform/account/modules/ec2/variables.tf
+++ terraform/account/modules/ec2/variables.tf
@@ -8,3 +8,13 @@ variable "target_group_arns" {
   description = "The target group ARNs from the ALB module"
   type        = set(string)
 }
+
+variable "alb_security_group" {
+  description = "The security group from the ALB"
+  type        = string
+}
+
+variable "default_aws_subnets" {
+  description = "The default subnets in AWS"
+  type        = set(string)
+}
