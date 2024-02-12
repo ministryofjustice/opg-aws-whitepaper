325a0325a82acfa6b097d2fd4be679e9d4f0ccfe
Jay Whitwell <72501756+jay-whitwell@users.noreply.github.com>
2024-02-12 00:16:34 +0000

    Ec2 (#53)
    * change cidr range to match vpc
    
    * rename vars for clarity
    
    * add things for clarity
    
    * use cidrsubnets
    
    * add vpc cidr block
    
    * extend by 8 bits to result in /24
    
    * use each.value

--- terraform/account/modules/alb/locals.tf
+++ terraform/account/modules/alb/locals.tf
@@ -3,12 +3,11 @@ locals {
   https = 443
   ssh   = 22
 
-  all_subnets = {
-    "a" = { cidr_block = "10.0.1.0/24" },
-    "b" = { cidr_block = "10.0.2.0/24" },
-    "c" = { cidr_block = "10.0.3.0/24" }
+  private_subnets = {
+    "a" = { cidr_block = "10.0.1.0/16" },
+    "b" = { cidr_block = "10.0.2.0/16" },
+    "c" = { cidr_block = "10.0.3.0/16" }
   }
 
-  # If the loadbalancer is public, we don't want to create unnecessary subnets
-  subnets = var.private ? local.all_subnets : {}
+  subnet_cidrs = var.public ? [] : cidrsubnets(var.vpc_cidr_block, 8, 8, 8)
 }
