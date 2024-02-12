13435963736fa3a832442e8b3794957995dcc051
Jay Whitwell <jay.whitwell@digital.justice.gov.uk>
2024-02-12 15:09:24 +0000

    add conditional

--- terraform/account/modules/ec2/outputs.tf
+++ terraform/account/modules/ec2/outputs.tf
@@ -1,7 +1,7 @@
 output "public_security_group_id" {
-  value = aws_security_group.public-inbound[0].id
+  value = var.public ? aws_security_group.public-inbound[0].id : ""
 }
 
 output "private_security_group_id" {
-  value = aws_security_group.private-inbound[0].id
+  value = var.public ? aws_security_group.private-inbound[0].id : ""
 }
