33d48535634cb226b976c50e88d05374b2ef4614
Jay Whitwell <jay.whitwell@digital.justice.gov.uk>
2024-02-13 11:05:16 +0000

    add try

--- terraform/account/modules/ec2/outputs.tf
+++ terraform/account/modules/ec2/outputs.tf
@@ -1,7 +1,7 @@
 output "public_security_group_id" {
-  value = var.public ? aws_security_group.public-inbound[0].id : ""
+  value = try(aws_security_group.public-inbound[0].id, "")
 }
 
 output "private_security_group_id" {
-  value = var.public ? aws_security_group.private-inbound[0].id : ""
+  value = try(aws_security_group.private-inbound[0].id, "")
 }
