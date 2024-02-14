e2c4ebf503f6c85609d15f47c9a1c2fe4b20dcc5
jay-whitwell <jay.whitwell@digital.justice.gov.uk>
2024-02-14 12:38:19 +0000

    fix data name

--- terraform/account/modules/alb/locals.tf
+++ terraform/account/modules/alb/locals.tf
@@ -4,9 +4,9 @@ locals {
   ssh   = 22
 
   private_subnets = {
-    subnet1 = { cidr = var.public ? null : cidrsubnet(data.aws_vpc.selected.cidr_block, 8, 0) },
-    subnet2 = { cidr = var.public ? null : cidrsubnet(data.aws_vpc.selected.cidr_block, 8, 1) },
-    subnet3 = { cidr = var.public ? null : cidrsubnet(data.aws_vpc.selected.cidr_block, 8, 2) }
+    subnet1 = { cidr = var.public ? null : cidrsubnet(data.aws_vpc.default.cidr_block, 8, 0) },
+    subnet2 = { cidr = var.public ? null : cidrsubnet(data.aws_vpc.default.cidr_block, 8, 1) },
+    subnet3 = { cidr = var.public ? null : cidrsubnet(data.aws_vpc.default.cidr_block, 8, 2) }
   }
 
   subnet_cidrs = var.public ? [] : cidrsubnets(aws_default_vpc.default.cidr_block, 8, 8, 8)
