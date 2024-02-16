561b3dae865ad2ad58ace10fd2e05c65a0b75359
Jay Whitwell <72501756+jay-whitwell@users.noreply.github.com>
2024-02-16 11:09:16 +0000

    Bugfix (#71)
    * change port
    
    * test app data script
    
    * increase health check time
    
    * add key pair for debug

--- python/app/api.py
+++ python/app/api.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 import uvicorn
 import random
+
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-  quotes = ["a", "b", "c"]
-  return { quotes[random.randrange(0, 3)]}
+    quotes = ["a", "b", "c"]
+    chosen_quote = random.choice(quotes)  # Use random.choice for simplicity
+    return {"quote": chosen_quote}
 
-uvicorn.run(app, port=8000)
+# Check if the script is run directly (not imported as a module)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
