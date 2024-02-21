ffd79e94d19810e068a99f72a20cef2863fe9b96
Jay Whitwell <jay.whitwell@digital.justice.gov.uk>
2024-02-21 11:05:12 +0000

    update python app

--- python/web/web.py
+++ python/web/web.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import requests
 import argparse
+import os
 
 app = Flask(__name__)
 
@@ -11,13 +12,14 @@ arg = parser.parse_args()
 
 @app.route('/')
 def home():
+  host = f"Host: {os.uname()[1]}"
   try:
     response = requests.get(arg.url)
     data = response.text
   except requests.RequestException as e:
     data = f"Error fetching data: {e}"
 
-  return render_template('index.html', data=data)
+  return render_template('index.html', data=data, host=host)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
