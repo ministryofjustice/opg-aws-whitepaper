3d96497ee433b56993c59dd47dbbeee4e7887ff1
Jay Whitwell <72501756+jay-whitwell@users.noreply.github.com>
2024-02-21 11:14:36 +0000

    Updates (#95)
    * run web only
    
    * fix
    
    * update python app
    
    * make the app public for testing
    
    * add

--- terraform/account/main.tf
+++ terraform/account/main.tf
@@ -5,6 +5,7 @@ module "network" {
   }
 }
 
+/*
 module "public-loadbalancer" {
   source             = "./modules/alb"
   vpc_id             = module.network.vpc.id
@@ -28,6 +29,37 @@ module "ec2-web" {
   subnet_ids         = module.network.public_subnet_ids
   server_port        = local.web_server_port
   cluster_name       = local.web_cluster_name
+  public             = true
+  app_server_port    = local.app_server_port
+  providers = {
+    aws = aws.sandbox
+  }
+}
+*/
+
+module "public-loadbalancer" {
+  source             = "./modules/alb"
+  vpc_id             = module.network.vpc.id
+  public             = true
+  subnet_ids         = module.network.public_subnet_ids
+  availability_zones = data.aws_availability_zones.default.names
+  server_port        = local.app_server_port
+  security_group     = module.ec2-app.public_security_group_id
+  cluster_name       = "public-${local.app_cluster_name}"
+
+  providers = {
+    aws = aws.sandbox
+  }
+}
+
+module "ec2-app" {
+  source             = "./modules/ec2"
+  alb_security_group = module.public-loadbalancer.alb_security_group_id
+  vpc_id             = module.network.vpc.id
+  target_group_arns  = module.public-loadbalancer.target_group_arns
+  subnet_ids         = module.network.public_subnet_ids
+  server_port        = local.app_server_port
+  cluster_name       = local.app_cluster_name
   public             = true
   app_server_port    = local.app_server_port
   providers = {
