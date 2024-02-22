a9f0bfd2a03e5df4d4220fe7782a11e2b384535a
Jay Whitwell <72501756+jay-whitwell@users.noreply.github.com>
2024-02-22 11:52:23 +0000

    Updates (#100)
    * test web
    
    * remove url
    
    * split
    
    * use new modules
    
    * try with new split

---
+++ terraform/account/modules/ec2-web/variables.tf
@@ -0,0 +1,47 @@
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+variable "target_group_arns" {
+  description = "The target group ARNs from the ALB module"
+  type        = set(string)
+}
+
+variable "alb_security_group" {
+  description = "The security group from the ALB"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "The subnets to use"
+  type        = set(string)
+}
+
+variable "cluster_name" {
+  description = "The name for the cluster of resources"
+  type        = string
+}
+
+variable "public" {
+  description = "Whether the EC2 instance is publicly accessible"
+  type        = bool
+  default     = false
+}
+
+variable "app_alb_fqdn" {
+  description = "The URL for the app server ALB"
+  type        = string
+  default     = ""
+}
+
+variable "vpc_id" {
+  description = "The VPC for the loadbalancer to use"
+  type        = string
+}
+
+variable "internal_loadbalancer_sg" {
+  description = "The security group of the internal loadbalancer"
+  type        = string
+}
